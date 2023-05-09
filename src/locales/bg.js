64a650876354ee51edc21e40b2fe30d791c1ed4b
Marin Terziyski <marin.terziyski@gmail.com>
2023-05-09 23:24:27 +0300

    adding formik, yuo, i18n

---
+++ src/locales/bg.js
@@ -0,0 +1,23 @@
+const bg = {
+    translation: {
+      loginPage: {
+        email: "Имейл",
+        emailValidation: "Моля въведете валиден имейл",
+        password: "Парола",
+        passwordValidation: "Паролата трябва да е минимум 6 символа",
+        login: 'Влез',
+        signUpText: "Нямате акаунт ?",
+        loginText: "Имате акаунт ?",
+        signUpButton: "Регистрирайте се",
+        register: 'Регистрация',
+        firstName: "Име",
+        lastName: "Фамилия",
+        requiredFieldValidation: "Това поле е задължително",
+        address: "Адрес",
+        addressValidation: "Моля въведете адрес",
+        repeatPassword: "Повтори паролата",
+        repeatPasswordValidation: "Паролата не съвпада"
+      },
+    },
+};
+export default bg;
